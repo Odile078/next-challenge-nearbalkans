4e663e00f4b04de52212bac15fb0f49e05fcea1a
Odile078 <ouwimpuhwe620@daviscollege.com>
2022-10-31 19:52:22 +0200

    Fixing Build errors on Links

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Hero = () => {
     return (
         <div className="bg-white px-[23px] lg:px-0 font-primary relative lg:pt-32 pt-12 h-[600px] lg:h-[500px] w-full">
@@ -7,11 +9,13 @@ const Hero = () => {
                 <h1 className="mx-auto text-4xl font-medium lg:text-7xl lg:w-full w-200 text-brandBlack"> Get<span className="not-italic text-transparent text-gradient w-fit bg-clip-text"> Near </span> Certified!</h1>
                 <p className="pt-5 pb-24 text-lg leading-7 lg:text-22 text-brandBlack-light">We, in NEAR Balkans, know how important it is to have regional and local support. Now you can become NEAR certified in a regional language of preference and connect with our team to support your further growth within the NEAR ecosystem.</p>
                 <div className="container mx-auto text-center">
-                    <a href="/#learning" className="cursor-pointer">                       
+                    <Link href="/#learning"><a  className="cursor-pointer">                       
                         <picture className="container flex items-center justify-center mx-auto text-center">
                             <img alt="arrow icon" src="/svgs/arrow-icon.svg" className="" />
                         </picture>
-                </a></div>
+                    </a>
+                    </Link>
+                </div>
             </div>
         </div>
     );
